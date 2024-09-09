d1f29862416c9ebe0b64f747e92732d119b8dbb3
KazukiMiyazato2021 <kazuki.miyazato.2021+git@gmail.com>
2024-09-10 01:20:13 +0900

    🐛 force copy files

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cp -ip .alias.git.sh .alias.sh .zsh_keybind.sh .zshprompt.sh .zshrc keybindings.json ~
+\cp -p .alias.git.sh .alias.sh .zsh_keybind.sh .zshprompt.sh .zshrc keybindings.json ~
